6832cd9b20e7fd0c39620cef0fa84f083b7c6c87
Keiden Byrne <keiden.byrne@stu.ocsb.ca>
2023-02-08 19:19:54 +0000

    Day 4: I know how to properly do style now

---
+++ HelloStyle.java
@@ -0,0 +1,30 @@
+/**
+* What I plan to do next year and how I feel about it.
+*
+* @author  Keiden B
+* @version 1.0
+* @since   2023-02-08
+*/
+
+public final class HelloStyle {
+    /**
+    * Neccessary to prevent HideUtilityClass Error.
+    *
+    * @exception IllegalStateException Utility class
+    * @see IllegalStateException
+    */
+    private HelloStyle() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+    * Main lines of code.
+    *
+    * @param args a placeholder value when making the main function
+    */
+    public static void main(String[] args) {
+        System.out.println("Next year, I'm going to Uni for Comp Sci.");
+        System.out.println("I'm pretty excited? Hoping for good "
+                         + "roommates if I have to live on campus.");
+    }
+}
