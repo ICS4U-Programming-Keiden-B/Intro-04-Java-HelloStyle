2b80b1cab247ba3c6cfe28920ae6693c0fc2ef83
Keiden Byrne <keiden.byrne@stu.ocsb.ca>
2023-02-14 18:57:57 +0000

    linter fix?

--- HelloStyle.java
+++ HelloStyle.java
@@ -1,7 +1,4 @@
-/**
- * Info about this package doing something for package-info.java file.
- */
-package HelloStyle;
+package hellostyle;
 
 /**
 * What I plan to do next year and how I feel about it.
